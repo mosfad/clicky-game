21603a481fb17b2751781a8286d23e5293aeb604
Dupe Fadina <maafadina@hotmail.com>
2019-08-31 23:46:11 -0700

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import ImageCard from "./component/ImageCard";
+import NavBar from "./component/NavBar";
+import Footer from "./component/Footer";
+import images from "./images.json";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    images
+  };
+
+  render() {
+    return (
+      <div>
+        <NavBar />
+        <ul className="list-group">
+          {this.state.images.map(item => (
+            <li className="list-group-item" key={item.id}>
+              <ImageCard key={item.id} id={item.id} image={item.image} />
+            </li>
+          ))}
+        </ul>
+        <Footer />
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/component/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer>
+      <div className="footer-copyright text-center py-3 bg-secondary">
+        <strong>© 2018 Copyright:</strong>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
