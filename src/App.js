98ad4f78502ac8b8c51edaef89cc64787f91cafb
Dupe Fadina <maafadina@hotmail.com>
2019-09-04 16:01:13 -0700

    Changed and successfully implemented reshuffle algorithm. Repetitions have been eliminated

--- src/App.js
+++ src/App.js
@@ -23,20 +23,20 @@ class App extends Component {
   };
 
   reshuffleImages = () => {
-    const arrInd = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
+    const arrIndex = [];
     const newImage = [];
-
-    while (arrInd.length !== 0) {
-      let randNum = Math.floor(Math.random() * 12);
-      if (!arrInd.includes(randNum)) continue;
-      //store the index for this easy access
-      let currInd = arrInd.indexOf(randNum);
-      //shuffle a random image into  a different position
-      newImage.push(this.state.images[currInd]); // I am here..............
-      //remove current index from the array index
-      arrInd.splice(currInd, 1);
+    let randNum;
+    //create a shuffled array of indices that will be used to create a random image array.
+    while (arrIndex.length < 12) {
+      randNum = Math.floor(Math.random() * 12);
+      //if random number is already in index array, then restart loop
+      if (arrIndex.includes(randNum)) continue;
+      arrIndex.push(randNum);
     }
-    console.log(newImage);
+    //create reshuffled array using arrIndex
+    for (let i = 0; i < arrIndex.length; i++) {
+      newImage.push(this.state.images[arrIndex[i]]);
+    }
     return newImage;
   };
 
@@ -104,3 +104,33 @@ class App extends Component {
 }
 
 export default App;
+
+/*THIS ALGORITHM DOESN'T WORK**************************************
+  altShuffleImages = () => {
+    const arrInd = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
+    const newImage = [];
+
+    while (arrInd.length !== 0) {
+      let randNum = Math.floor(Math.random() * 12);
+      console.log("Random number generated is " + randNum);
+      if (!arrInd.includes(randNum)) continue;
+      //store the index for this easy access
+      let currInd = arrInd.indexOf(randNum);
+      console.log("Current index is " + currInd);
+      //shuffle a random image into  a different position
+      console.log("Before push, image was: ");
+      console.log(newImage);
+      newImage.push(
+        this.state.images[currInd]
+      ); //Algorithm here is wrong.
+      //Maybe I need a check here to make sure the image being added isn't a repeat
+      console.log("After push, image is: ");
+      console.log(newImage);
+      //remove current index from the array index
+      arrInd.splice(currInd, 1); //Algorithm here is wrong?
+      console.log("array index is ");
+      console.log(arrInd);
+    }
+    console.log(newImage);
+    return newImage;
+  };**********************************************************************/
