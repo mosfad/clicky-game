ce1963eef3252e0d4f1166329d9f2a562fa102c5
Dupe Fadina <maafadina@hotmail.com>
2020-03-30 02:18:09 -0700

    Updates

---
+++ precache-manifest.01e8bc0e00ee4295ac489be540dff6e7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ae5bb7c164491148b7eb90d5a0e191d",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "4bdbf7d918d488b080fc",
+    "url": "/clicky-game/static/css/2.89f067aa.chunk.css"
+  },
+  {
+    "revision": "536552e9194ead84e929",
+    "url": "/clicky-game/static/css/main.413ed158.chunk.css"
+  },
+  {
+    "revision": "4bdbf7d918d488b080fc",
+    "url": "/clicky-game/static/js/2.de28f938.chunk.js"
+  },
+  {
+    "revision": "21ced859ea2b2d6b856d461ad6c2afed",
+    "url": "/clicky-game/static/js/2.de28f938.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "536552e9194ead84e929",
+    "url": "/clicky-game/static/js/main.b59b9c4c.chunk.js"
+  },
+  {
+    "revision": "c81afc240fbac3d8aeb5",
+    "url": "/clicky-game/static/js/runtime-main.4e8dbddb.js"
+  },
+  {
+    "revision": "a488a209305c82dd86d7442c93ab6fcb",
+    "url": "/clicky-game/static/media/pattern-728509_1920.a488a209.png"
+  }
+]);
